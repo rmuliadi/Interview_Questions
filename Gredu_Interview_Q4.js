e9bec31bbf24e1880f6eefc89d146049431458e9
rmuliadi <rmuliadi@users.noreply.github.com>
2017-11-03 14:25:37 +0700

    Add files via upload

--- Gredu_Interview_Q4.js
+++ Gredu_Interview_Q4.js
@@ -5,26 +5,10 @@ var prevTime = 0; // Previous Time
 var curTime = 0; // Current Time
 var deltaTime = 0; // The difference of time
 
-// Timing from 50 ms to 100 ms
-function getTime () {
-	prevTime = (new Date()).getTime();
-	curTime = prevTime;
-	return deltaTime = curTime - prevTime;
-
-}
-
 
 // asynchronous code
 function printOutResult () {
-	//var d = new Date.now();
-	//var n = d.getMilliseconds();
-	//if (n > delay1) {
-	//	document.writeln(n);
-	//	if (n < delay2) {
-	//		documentwriteln(n);
-			timer = setInterval(document.writeln(string), delay1, delay2);
-	//	}
-	//}
+	timer = setInterval(console.log(string), delay1, delay2);
 	clearInterval(timer);
 	return timer;
 }
@@ -32,10 +16,7 @@ function printOutResult () {
 
 
 
-
+// Promise function to resolve the result string
 var myPromise = new Promise(function(resolve, reject) {
-	if ((deltaTime >  delay1) && (deltaTime < delay2)) 
 		resolve(printOutResult());
-	else
-		reject(printOutResult());
 })
